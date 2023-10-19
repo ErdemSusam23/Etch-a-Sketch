37e0a092d3eedc182eff29f1f5b4cb611bca8b39
ErdemSusam23 <ersusam2003@gmail.com>
2023-10-19 18:27:53 +0300

    add 16x16 grid with style

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container');
 function makeDivs(numDivs) {
 	for (let index = 0; index < numDivs; index++) {
 		let cell = document.createElement('div');
-		container.appendChild(cell);
+		container.appendChild(cell).className = 'grid-item';
 	}
 }
 
