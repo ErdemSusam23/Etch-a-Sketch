366a5c9eff49101d5394e758af6c1b9fb7284b55
ErdemSusam23 <ersusam2003@gmail.com>
2023-10-26 20:12:45 +0300

    add button for getting canvas size from user and clear button

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const container = document.querySelector('.grid-container');
+const menu = document.querySelector('.left-menu');
+const clearButton = document.createElement('button');
+const canvasButton = document.createElement('button');
 let cell = document.createElement('div');
 
+canvasButton.innerText = 'Enter Size';
+
+clearButton.classList.add('clear');
+clearButton.innerText = 'CLEAR';
+
+menu.appendChild(canvasButton);
+menu.appendChild(clearButton);
+
 let mouseDown = false;
+let canvasSize;
+
+checkMouse();
+
+canvasButton.addEventListener('click', function () {
+	container.innerHTML = '';
+	canvasSize = parseInt(prompt('Enter The Size of Canvas'));
+	checkCanvasSize(canvasSize);
+	console.log(typeof canvasSize);
+});
+
+clearButton.addEventListener('click', function () {
+	container.innerHTML = '';
+});
 
 function createCanvas(numDivs) {
 	container.style.setProperty('--grid-column', numDivs);
@@ -26,5 +51,12 @@ function checkMouse() {
 	container.onmouseup = () => (mouseDown = false);
 }
 
-checkMouse();
-createCanvas(16);
+function checkCanvasSize(size) {
+	if (size > 100) {
+		canvasSize = parseInt(
+			prompt('Size cannot be bigger than 100 please enter again')
+		);
+	} else {
+		createCanvas(canvasSize);
+	}
+}
