73cfc0efd4008cbf1373d41789e0d70053681d28
ErdemSusam23 <ersusam2003@gmail.com>
2023-10-26 16:40:29 +0300

    change color on hover

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid-container');
+let cell = document.createElement('div');
 
-function makeDivs(numDivs) {
-	for (let index = 0; index < numDivs; index++) {
+let mouseDown = true;
+
+function createCanvas(numDivs) {
+	for (let index = 0; index < numDivs * numDivs; index++) {
 		let cell = document.createElement('div');
+		cell.addEventListener('mouseover', changeColor(cell));
 		container.appendChild(cell).className = 'grid-item';
 	}
 }
 
-makeDivs(256);
+function changeColor(element) {
+	if (mouseDown) {
+		element.addEventListener('mouseover', function () {
+			element.style.backgroundColor = 'red';
+		});
+	}
+}
+
+function checkMouse() {
+	document.body.addEventListener('onmousedown', function () {
+		mouseDown = true;
+	});
+	document.body.addEventListener('onmouseup', function () {
+		mouseDown = false;
+	});
+	console.log(mouseDown);
+}
+
+checkMouse();
+createCanvas(16);
